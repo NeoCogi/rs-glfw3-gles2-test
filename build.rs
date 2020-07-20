764313a2493fb0822c354c74074403976b2e7402
Wael El Oraiby <wael.eloraiby@gmail.com>
2020-07-19 20:19:12 -0400

    Remove warnings from build.rs

--- build.rs
+++ build.rs
@@ -1,18 +1,8 @@
 
 use std::env;
-use std::fs::*;
-use std::fmt::Write;
-use std::io::Write as OtherWrite;
 
 fn main() {
     let target = env::var("TARGET");
-    println!("cargo:warning=Building for {:?} target!", target);
-    // match target {
-    //     Ok(s) if s == "wasm32-unknown-emscripten" => {
-    //         println!("cargo:rustc-cdylib-link-arg=-s");
-    //         println!("cargo:rustc-cdylib-link-arg=USE_GLFW=3");
-    //     },
-    //     _ => ()
-    // }
+    println!("cargo:info=Building for {:?} target!", target);
     println!("cargo:rustc-link-lib=c"); // the "-l" flag
 }
